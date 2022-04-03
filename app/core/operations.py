94bf7c7a4d1fb1d0f79d9a82e63bf019e33a8b5e
Dinu Mihai <mihai.dinu93@gmail.com>
2022-04-03 17:46:10 +0300

    add result caching for operations

--- app/core/operations.py
+++ app/core/operations.py
@@ -1,7 +1,12 @@
+from functools import lru_cache
+
+
+@lru_cache
 def power(a, b):
     return pow(a, b)
 
 
+@lru_cache
 def n_th_fibonacci(n):
     a, b = 0, 1
     c = 1
@@ -12,6 +17,7 @@ def n_th_fibonacci(n):
     return a
 
 
+@lru_cache
 def factorial(n):
     p = 1
     for i in range(1, n + 1):
